16c68034f710b48de5f7a981c9205d90b8755693
Yerassyl Tokenov <erasyl.tokenov.94@mail.ru>
2022-12-21 17:40:20 +0600

    2 st attempt

--- sedawk.sh
+++ sedawk.sh
@@ -15,11 +15,11 @@ mv passwd_tmp passwd_new
 sed -i '/avahi:/s/\/usr\/sbin\/nologin/\/bin\/bash/' passwd_new
 
 #d. Save only 1-st 3-dr 5-th 7-th columns of each string based on : delimiter
-awk -F: '{ $1":"$3":"5":"7; print }' passwd_new > passwd_tmp
+awk -F: '{ print $1 ":" $3 ":" $5 ":" $7 }' passwd_new > passwd_tmp
 mv passwd_tmp passwd_new
 
 #e. Remove all lines from file containing word daemon
-sed '/daemon/d' passwd_new
+sed -i '/daemon/d' passwd_new
 
 #f. Change shell for all users with even UID to /bin/zsh
 awk -F: 'BEGIN {OFS=":"} $2 % 2 == 0 { $4 = "/bin/bash" } {print}' passwd_new > passwd_tmp
